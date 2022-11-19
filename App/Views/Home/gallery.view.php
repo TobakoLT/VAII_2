45d65fc0be4fb8f279826172debfb8de3c1b3f05
TobakoLT <martin.hronec41@gmail.com>
2022-11-19 21:30:37 +0100

    Zmena obrázku

--- App/Views/Home/gallery.view.php
+++ App/Views/Home/gallery.view.php
@@ -2,15 +2,15 @@
 
         <h2 class="mt-2"><b>Galéria obrázkov</b></h2>
         <p>Kliknite pre zvačšenie.</p>
-    <div class="col-md-1 mb-3">
-            <a class="btn btn-warning border-dark border-2" href="?c=posts" role="button">UPLOAD HERE</a>
+    <div class="col-md-2 mb-3">
+            <a class="btn btn-warning border-dark border-2" href="?c=posts" role="button">Nahraj si vlastný obrázok</a>
     </div>
 
     <div class="row border-img">
 
         <div class="col-md-4">
             <div class="mb-md-3 border border-white border-2 rounded mb-2">
-                <a href="img/cross1.jpg" target="_blank" >
+                <a href="public/images/cross1.jpg" target="_blank" >
                     <img src="public/images/cross1.jpg" alt="moto1" class="image-mask w-100 mb-0 rounded img-fluid">
 
                 </a>
@@ -18,14 +18,14 @@
         </div>
         <div class="col-md-4">
             <div class="mb-md-3 border border-white border-2 rounded mb-2">
-                <a class="" href="img/cross2.jpg" target="_blank">
+                <a class="" href="public/images/cross2.jpg" target="_blank">
                     <img src="public/images/cross2.jpg" alt="moto2" class="w-100 mb-0 shadow-1-strong rounded img-fluid">
                 </a>
             </div>
         </div>
         <div class="col-md-4">
             <div class="mb-md-3 border border-white border-2 rounded mb-2">
-                <a href="img/cross3.jpg" target="_blank">
+                <a href="public/images/cross3.jpg" target="_blank">
                     <img src="public/images/cross3.jpg" alt="moto3" class="w-100 mb-0 shadow-1-strong rounded img-fluid">
 
                 </a>
@@ -33,14 +33,14 @@
         </div>
         <div class="col-md-4">
             <div class="mb-md-3 border border-white border-2 rounded mb-2">
-                <a href="img/cross4.jpg" target="_blank">
+                <a href="public/images/cross4.jpg" target="_blank">
                     <img src="public/images/cross4.jpg" alt="moto4" class="w-100 mb-0 shadow-1-strong rounded img-fluid">
                 </a>
             </div>
         </div>
         <div class="col-md-4">
             <div class="mb-md-3 border border-white border-2 rounded mb-2">
-                <a href="img/cross5.jpg" target="_blank">
+                <a href="public/images/cross5.jpg" target="_blank">
                     <img src="public/images/cross5.jpg" alt="moto5" class="w-100 mb-0 shadow-1-strong rounded img-fluid">
                 </a>
             </div>
@@ -54,21 +54,21 @@
         </div>
         <div class="col-md-4">
             <div class="mb-md-3 border border-white border-2 rounded mb-2">
-                <a href="img/cross7.jpg" target="_blank">
+                <a href="public/images/cross7.jpg" target="_blank">
                     <img src="public/images/cross7.jpg" alt="moto7" class="w-100 mb-0 shadow-1-strong rounded img-fluid">
                 </a>
             </div>
         </div>
         <div class="col-md-4">
             <div class="mb-md-3 border border-white border-2 rounded mb-2">
-                <a href="img/cross8.jpg" target="_blank">
+                <a href="public/images/cross8.jpg" target="_blank">
                     <img src="public/images/cross8.jpg" alt="moto8" class="w-100 mb-0 shadow-1-strong rounded img-fluid">
                 </a>
             </div>
         </div>
         <div class="col-md-4">
             <div class="mb-md-3 border border-white border-2 rounded mb-2">
-                <a href="img/cross9.jpg" target="_blank">
+                <a href="public/images/cross9.jpg" target="_blank">
                     <img src="public/images/cross9.jpg" alt="moto9" class="w-100 mb-0 shadow-1-strong rounded img-fluid">
                 </a>
             </div>
