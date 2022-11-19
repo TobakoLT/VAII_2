b79bc4e313e5ac3f0f036ef306df2cf06eff4e05
TobakoLT <martin.hronec41@gmail.com>
2022-11-19 21:24:17 +0100

    Pridaný model pre Post

---
+++ App/Models/Post.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+
+class Post extends Model
+{
+    protected $id;
+    protected $text;
+    protected $img;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text): void
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * @param mixed $img
+     */
+    public function setImg($img): void
+    {
+        $this->img = $img;
+    }
+
+
+}
