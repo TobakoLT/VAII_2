b820c9b413e7fbd0009315d9f8c63eabc1e73498
TobakoLT <martin.hronec41@gmail.com>
2022-11-21 13:28:07 +0100

    Úprava navbar-u

--- App/Views/root.layout.view.php
+++ App/Views/root.layout.view.php
@@ -19,44 +19,45 @@
     <link rel="icon" type="image/x-icon" href="public/images/motologo1.png">
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-color border border-danger border-opacity-25 border-1">
+<nav class="navbar navbar-expand-md bg-color border border-danger border-opacity-25 border-1">
     <div class="container-fluid">
         <a class="navbar-brand" href="?c=home">
-            <img src="public/images/motologo1.png" title="<?= \App\Config\Configuration::APP_NAME ?>">
+            <img src="public/images/motologo1.png" alt="" title="<?= \App\Config\Configuration::APP_NAME ?>">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav sticky-top font-size mx-auto mb-2 mb-sm-0">
+            <ul class="navbar-nav sticky-top font-size ms-auto mb-2 mb-sm-0">
                 <li class="nav-item" >
                     <a class="nav-link active menu-button rounded me-1" aria-current="page" href="?c=home&a=index">ÚVOD</a>
                 </li>
                 <li class="nav-item" >
-                    <a class="nav-link menu-button rounded me-1" href="?c=home&a=gallery">GALÉRIA</a>
+                    <a class="nav-link active menu-button rounded me-1" href="?c=home&a=gallery">GALÉRIA</a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link menu-button rounded me-1" href="?c=home&a=info">INFORMÁCIE</a>
+                    <a class="nav-link active menu-button rounded me-1" href="?c=home&a=info">INFORMÁCIE</a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link menu-button rounded" href="?c=home&a=contact">KONTAKT</a>
+                    <a class="nav-link active menu-button rounded" href="?c=home&a=contact">KONTAKT</a>
                 </li>
             </ul>
+            <?php if ($auth->isLogged()) { ?>
+                <ul class="navbar-nav ms-auto">
+                    <span class="navbar-text">Prihlásený používateľ: <b><?= $auth->getLoggedUserName() ?></b></span>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?c=auth&a=logout">Odhlásenie</a>
+                    </li>
+                </ul>
+            <?php } else { ?>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= \App\Config\Configuration::LOGIN_URL ?>">Prihlásenie</a>
+                    </li>
+                </ul>
+            <?php } ?>
         </div>
-        <?php if ($auth->isLogged()) { ?>
-            <span class="navbar-text">Prihlásený používateľ: <b><?= $auth->getLoggedUserName() ?></b></span>
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="?c=auth&a=logout">Odhlásenie</a>
-                </li>
-            </ul>
-        <?php } else { ?>
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= \App\Config\Configuration::LOGIN_URL ?>">Prihlásenie</a>
-                </li>
-            </ul>
-        <?php } ?>
+
     </div>
 </nav>
 <div class="container-fluid mt-3">
