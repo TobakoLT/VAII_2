881d6440b26ffc4ff64226ef1fffc4fb3a2499ac
TobakoLT <martin.hronec41@gmail.com>
2023-01-04 01:27:43 +0100

    Pridanie modelu pre tabuľku users.

---
+++ App/Models/User.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+
+class User extends Model
+{
+    protected ?int $id = 0;
+    protected ?string $login = "";
+    protected ?string $email = "";
+    protected ?string $meno = "";
+    protected ?string $password_hash = "";
+    protected ?string $created_at = "";
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param string|null $login
+     */
+    public function setLogin(?string $login): void
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     */
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMeno(): ?string
+    {
+        return $this->meno;
+    }
+
+    /**
+     * @param string|null $meno
+     */
+    public function setMeno(?string $meno): void
+    {
+        $this->meno = $meno;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPasswordHash(): ?string
+    {
+        return $this->password_hash;
+    }
+
+    /**
+     * @param string|null $password_hash
+     */
+    public function setPasswordHash(?string $password_hash): void
+    {
+        $this->password_hash = $password_hash;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCreatedAt(): ?string
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param string|null $created_at
+     */
+    /*
+    public function setCreatedAt(?string $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+    */
+
+}
