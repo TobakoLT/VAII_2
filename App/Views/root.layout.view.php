30c84fe9bfe8698684b4e4c6364641b604335648
TobakoLT <martin.hronec41@gmail.com>
2023-01-07 15:45:25 +0100

    Úprava navbaru.

--- App/Views/root.layout.view.php
+++ App/Views/root.layout.view.php
@@ -11,24 +11,28 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
             crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
             crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.6/clipboard.min.js"></script>
     <link rel="stylesheet" href="public/css/styl.css">
     <script src="public/js/script.js"></script>
     <link rel="icon" type="image/x-icon" href="public/images/motologo1.png">
 </head>
 <body>
 <nav class="navbar navbar-expand-md bg-color border border-danger border-opacity-25 border-1">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="?c=home">
-            <img src="public/images/motologo1.png" alt="" title="<?= \App\Config\Configuration::APP_NAME ?>">
-        </a>
+    <div class="container-fluid ">
+        <div class="col-1">
+            <a class="navbar-brand" href="?c=home">
+                <img src="public/images/motologo1.png" alt="" title="<?= \App\Config\Configuration::APP_NAME ?>">
+            </a>
+        </div>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav sticky-top font-size ms-auto mb-2 mb-sm-0">
+            <ul class="navbar-nav sticky-top font-size ms-auto mb-2 mb-sm-0 ">
                 <li class="nav-item " >
                     <a class="nav-link active menu-button rounded me-1" aria-current="page" href="?c=home&a=index">ÚVOD</a>
                 </li>
@@ -51,7 +55,7 @@
                 </ul>
             <?php } else { ?>
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
+                    <li class="nav-item ">
                         <a class="nav-link" href="<?= \App\Config\Configuration::LOGIN_URL ?>">Prihlásenie</a>
                     </li>
                     <li class="nav-item">
