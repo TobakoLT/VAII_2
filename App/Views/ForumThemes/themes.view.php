5eb90a7c001687d398d9f0be5c8da29bdefe2204
TobakoLT <martin.hronec41@gmail.com>
2023-01-13 02:03:41 +0100

    View pre zobrazenie tém z tabuľky.

--- App/Views/ForumThemes/themes.view.php
+++ App/Views/ForumThemes/themes.view.php
@@ -4,31 +4,27 @@ use App\Models\ForumTheme;
 /** @var ForumTheme[] $data */
 ?>
 <div class="container my-5 bg-white rounded">
-    <h1 class="text-center">Témy fóra</h1>
+    <h1 class="text-center pt-2">Témy fóra</h1>
+    <?php
+    foreach ($data as $forumTheme) {
+    ?>
     <div class="row">
         <div class="col-md-8">
             <div class="card mb-3">
                 <div class="card-header">
-                    <h5>Téma 1</h5>
+                    <h5><?php echo $forumTheme->getNazov() ?></h5>
                 </div>
                 <div class="card-body">
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                    <p class="card-text"><?php echo $forumTheme->getPopis() ?></p>
                     <a href="#" class="btn btn-primary">Zobraziť tému</a>
                 </div>
             </div>
-            <div class="card mb-3">
-                <div class="card-header">
-                    <h5>Téma 2</h5>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                    <a href="#" class="btn btn-primary">Zobraziť tému</a>
-                </div>
-            </div>
+
             <div class="col-md-4">
-                <button class="btn btn-primary my-3" href="?c=forumPosts&a=forumposts">Pridať tému</button>
-                <a class="nav-link active menu-button rounded" href="?c=forumPosts&a=posts">FÓRUM</a>
+                <?php if($auth->isLogged()) {?>
+                <a class="btn btn-primary my-3" href="?c=forumThemes&a=create">Nová téma.</a>
+                <?php } ?>
             </div>
         </div>
     </div>
-
+<?php } ?>
