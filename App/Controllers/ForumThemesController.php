054b1d0af34895337f6bc7c73a45e09033076f35
TobakoLT <martin.hronec41@gmail.com>
2023-01-13 02:02:20 +0100

    Prvotný kontroler pre témy fóra.

--- App/Controllers/ForumThemesController.php
+++ App/Controllers/ForumThemesController.php
@@ -14,6 +14,27 @@ use Exception;
  */
 class ForumThemesController extends AControllerBase
 {
+    /**
+     * @param $action
+     * @return bool
+     */
+    public function authorize($action): bool
+    {
+        switch ($action) {
+            case "delete":
+            case "create":
+            case "store":
+            case "edit":
+                return $this->app->getAuth()->isLogged();
+
+        }
+        return true;
+    }
+
+    /**
+     * @return Response
+     * @throws Exception
+     */
     public function index(): Response
     {
 
@@ -21,8 +42,61 @@ class ForumThemesController extends AControllerBase
         return $this->html($forumThemes);
     }
 
+    /**
+     * @return Response
+     */
     public function themes(): Response
     {
         return $this->html();
+    }
+
+    /**
+     * @return \App\Core\Responses\RedirectResponse
+     * @throws Exception
+     */
+    public function delete()
+    {
+        $id = $this->request()->getValue('id');
+        $themeToDelete = ForumTheme::getOne($id);
+        if ($themeToDelete) {
+            $themeToDelete->delete();
+        }
+        return $this->redirect("?c=forumThemes&a=themes");
+    }
+
+    /**
+     * @return \App\Core\Responses\RedirectResponse
+     * @throws Exception
+     */
+    public function store()
+    {
+        $id = $this->request()->getValue('id');
+        $theme = ($id ? ForumTheme::getOne($id) : new ForumTheme());
+
+        $theme->setNazov($this->request()->getValue('title'));
+        $theme->setCreatedAt(date("Y-m-d"));
+        $theme->setPopis($this->request()->getValue('description'));
+
+        $theme->save();
+        return $this->redirect("?c=forumthemes");
+    }
+
+    /**
+     * @return \App\Core\Responses\ViewResponse
+     */
+    public function create()
+    {
+        return $this->html(new ForumTheme(), viewName: 'create');
+    }
+
+    /**
+     * @return \App\Core\Responses\ViewResponse
+     * @throws Exception
+     */
+    public function edit()
+    {
+        $id = $this->request()->getValue('id');
+        $themeToEdit = ForumTheme::getOne($id);
+        return $this->html($themeToEdit, viewName: 'create');
     }
 }
