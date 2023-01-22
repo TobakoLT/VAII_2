74cf12d8402b4efb757210c001d4ed05a470c55a
TobakoLT <martin.hronec41@gmail.com>
2023-01-22 17:51:15 +0100

    Edit.

--- App/Views/ForumPosts/create.form.view.php
+++ App/Views/ForumPosts/create.form.view.php
@@ -1,28 +1,30 @@
 <?php
 /** @var int $id */
+
 /** @var ForumPost $data */
 
 use App\Models\ForumPost;
 
 ?>
 <a href="?c=forumPosts&a=index&themeId=<?= $data['id'] ?>" class="btn btn-secondary">Naspäť</a>
-<div class="container my-5 pt-3 bg-white rounded">
+<div class="container my-5 pt-3 bg-white rounded border border-dark">
     <h1 class="text-center">Pridaj príspevok do fóra</h1>
     <form action="?c=forumPosts&a=store" method="post" enctype="multipart/form-data">
 
         <?php if ($data['post']->getId()) { ?>
             <input type="hidden" name="id" value="<?php echo $data['post']->getId() ?>">
-            <input type="hidden" name="theme_id" value="<?php echo $data['post']->getThemeId()?>">
+            <input type="hidden" name="theme_id" value="<?php echo $data['post']->getThemeId() ?>">
         <?php } else { ?>
             <input type="hidden" name="theme_id" value="<?php echo $data['id'] ?>">
         <?php } ?>
         <div class="form-group">
             <label class="pb-1" for="post_text">Text príspevku:</label>
-            <textarea class="form-control" id="post_text" name="post_text" rows="3" required autofocus></textarea>
+            <textarea class="form-control" id="post_text" name="post_text" rows="3" minlength="50" required autofocus></textarea>
         </div>
         <div class="form-group pt-2">
             <label for="author">Autor:</label>
-            <input type="text" class="form-control" id="author" name="author" value="<?php echo $_SESSION["user"]->getMeno(); ?>" disabled>
+            <input type="text" class="form-control" id="author" name="author" style="font-weight: bold"
+                   value="<?php echo $_SESSION["user"]->getMeno(); ?>" disabled>
         </div>
         <div class="form-group pt-3">
             <label for="attachment_image">Príloha:</label>
--- App/Views/ForumThemes/create.form.view.php
+++ App/Views/ForumThemes/create.form.view.php
@@ -1,6 +1,6 @@
-<button class="btn btn-secondary" onclick="window.location.href='?c=forumThemes'">Zoznam tém</button>
+<button class="btn btn-secondary mb-3" onclick="window.location.href='?c=forumThemes'">Zoznam tém</button>
 <div class="container bg-white p-5 rounded">
-    <h2>Vytvorte novú tému.</h2>
+    <h2>Vytvorte novú tému</h2>
     <form action="?c=forumThemes&a=store" method="post" enctype="multipart/form-data">
         <?php /** @var \App\Models\ForumTheme $data */
         if ($data->getId()) { ?>
@@ -9,13 +9,15 @@
         <div class="form-group row">
             <label for="title" class="col-12 col-md-3 col-form-label">Názov</label>
             <div class="col-12 col-md-9">
-                <input type="text" class="form-control" id="title" name="title" placeholder="Zadajte názov" minlength="3" maxlength="80" required autofocus>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Zadajte názov"
+                       minlength="3" maxlength="80" required autofocus>
             </div>
         </div>
         <div class="form-group row">
             <label for="description" class="col-12 col-md-3 col-form-label">Popis</label>
             <div class="col-12 col-md-9 mt-1">
-                <textarea class="form-control" id="description" name="description" rows="3" placeholder="Krátky popis čím sa daná téma zaoberá"></textarea>
+                <textarea class="form-control" id="description" name="description" rows="3"
+                          placeholder="Krátky popis čím sa daná téma zaoberá" required></textarea>
             </div>
         </div>
         <div class="form-group row">
