9b417b236d85476d28102f258004f726053cb148
TobakoLT <martin.hronec41@gmail.com>
2023-01-22 17:52:37 +0100

    Pridanie min a max dĺžky v input-och.

--- App/Views/Posts/create.form.view.php
+++ App/Views/Posts/create.form.view.php
@@ -1,25 +1,30 @@
+<?php /** @var \App\Models\Post $data */ ?>
+
 <div class="row mb-2 g-2">
     <div class="col-lg-6 p-0 box">
 
-        <form method="post" action="?c=posts&a=store" class="mb-md-3 border border-white border-2 rounded mb-2 bg-color-white"
+        <form method="post" action="?c=posts&a=store"
+              class="mb-md-3 border border-white border-2 rounded mb-2 bg-color-white"
               enctype="multipart/form-data">
 
-            <?php /** @var \App\Models\Post $data */
-            if ($data->getId()) { ?>
+            <?php if ($data->getId()) { ?>
                 <input type="hidden" name="id" value="<?php echo $data->getId() ?>">
             <?php } ?>
 
             <div class="mb-3">
                 <label for="FormName" class="form-label">Meno autora</label>
-                <input type="text" style="font-weight: bold" class="form-control" id="FormName" value="<?php echo $_SESSION["user"]->getMeno() ?>" readonly>
+                <input type="text" style="font-weight: bold" class="form-control" id="FormName"
+                       value="<?php echo $_SESSION["user"]->getMeno() ?>" readonly>
             </div>
 
             <label>Nadpis:
-                <input class="form-control" required size="120%" type="text" name="nadpis" value="<?php echo $data->getNadpis() ?>">
+                <input class="form-control" size="120%" type="text" name="nadpis"
+                       value="<?php echo $data->getNadpis() ?>" minlength="10" maxlength="50" required>
             </label>
 
             <label>Text clánku:
-                <input required class="rounded form-control" size="120%" type="text" name="clanok" value="<?php echo $data->getClanok() ?>">
+                <input class="rounded form-control" size="120%" type="text" name="clanok"
+                       value="<?php echo $data->getClanok() ?>" required minlength="100" maxlength="1000">
             </label>
 
             <label>Obrázok:
