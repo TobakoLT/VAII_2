681409f48f1625a1f39d1854e4d814486cac2428
TobakoLT <martin.hronec41@gmail.com>
2023-02-13 23:33:01 +0100

    Úprava vzhľadu formuláru, zamedzenie odoslania pri chybných vstupoch.

--- App/Views/Posts/create.form.view.php
+++ App/Views/Posts/create.form.view.php
@@ -1,39 +1,41 @@
 <?php /** @var \App\Models\Post $data */ ?>
 
-<div class="row mb-2 g-2">
-    <div class="col-lg-6 p-0 box">
+<a href="?c=posts" class="btn btn-secondary">Naspäť</a>
+<div class="container my-5 pt-3 bg-white rounded border border-dark">
+    <h1 class="text-center">Pridaj článok</h1>
+    <form method="post" action="?c=posts&a=store" enctype="multipart/form-data">
 
-        <form method="post" action="?c=posts&a=store"
-              class="mb-md-3 border border-white border-2 rounded mb-2 bg-color-white"
-              enctype="multipart/form-data">
+        <?php if ($data->getId()) { ?>
+            <input type="hidden" name="id" value="<?php echo $data->getId() ?>">
+        <?php } ?>
 
-            <?php if ($data->getId()) { ?>
-                <input type="hidden" name="id" value="<?php echo $data->getId() ?>">
-            <?php } ?>
-
-            <div class="mb-3">
-                <label for="FormName" class="form-label">Meno autora</label>
-                <input type="text" style="font-weight: bold" class="form-control" id="FormName"
-                       value="<?php echo $_SESSION["user"]->getMeno() ?>" disabled>
-            </div>
-
-            <label>Nadpis:
-                <input class="form-control" size="120%" type="text" name="nadpis"
-                       value="<?php echo $data->getNadpis() ?>" minlength="10" maxlength="50" required>
-            </label>
-
+        <div class="form-group">
+            <label for="FormName" class="form-label">Meno autora:</label>
+            <input type="text" style="font-weight: bold" class="form-control" id="FormName"
+                   value="<?php echo $_SESSION["user"]->getMeno() ?>" disabled>
+        </div>
+        <div class="form-group">
+            <label class="form-label">Nadpis:</label>
+            <input class="form-control" type="text" name="nadpis"
+                   value="<?php echo $data->getNadpis() ?>" minlength="10" maxlength="50" required>
+        </div>
+        <div class="form-group pt-2">
             <label>Text článku:</label>
             <textarea class="rounded form-control" rows="7" cols="50" name="clanok" required minlength="100"
                       maxlength="1000"><?php echo $data->getClanok() ?></textarea>
-
+        </div>
+        <div class="form-group pt-3">
             <label>Obrázok:</label>
             <div class="text-danger" id="img-input-error"></div>
             <input class="form-control rounded" formenctype="multipart/form-data" type="file" name="photo" id="photo">
-            <input class="rounded mt-1" type="submit" value="Odoslať">
-        </form>
+        </div>
+        <div class="form-group d-flex justify-content-center">
+            <button id="myButton" type="submit" class="btn btn-dark my-3">Odoslať</button>
+        </div>
+    </form>
 
-    </div>
 </div>
+
 
 <script>
     imageInput2();
