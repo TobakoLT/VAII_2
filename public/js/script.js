b0a8d05a895095236784906c3bcf711bc99511eb
TobakoLT <martin.hronec41@gmail.com>
2023-02-13 23:40:45 +0100

    Metódy na kontrolu platnosti údajov pri registrácií a úprave účtu.

--- public/js/script.js
+++ public/js/script.js
@@ -28,10 +28,6 @@ function displayConductModal() {
     });
 
 }
-*/
-function passwordStrengthOld() {
-    const passwordInput = document.getElementById('password');
-    const passwordStrengthIndicator = document.getElementById('password-strength-indicator');
 
 function imageInput() {
     const input = document.getElementById("photo");
@@ -43,10 +39,9 @@ function imageInput() {
             alert("Nepodporovaný typ súboru! Prosím vyberte obrázok (JPG, JPEG, PNG, GIF alebo WEBP! )");
         } else {
 
-    function checkPasswordStrength() {
-        const password = passwordInput.value;
-        const strengthRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
-        const mediumRegex = new RegExp("^(?=.{8,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+        }
+    });
+}
 
 function imageInput2() {
     const input = document.getElementById("photo");
@@ -56,16 +51,117 @@ function imageInput2() {
             errorDiv.innerHTML = "";
             document.getElementById("myButton").disabled = false;
         } else {
-            if (mediumRegex.test(password)) {
-                passwordStrengthIndicator.innerHTML = '<div class="strength medium">Stredne silné</div>'
+            const file = input.files[0];
+            const allowedExtensions = [".jpg", ".jpeg", ".png", ".gif", ".webp", ".jfif"];
+            const extension = file.name.slice(file.name.lastIndexOf("."));
+            if (!allowedExtensions.includes(extension)) {
+                errorDiv.innerHTML = "Nepodporovaný typ súboru! Prosím vyberte obrázok!";
+                document.getElementById("myButton").disabled = true;
             } else {
-                passwordStrengthIndicator.innerHTML = '<div class="strength weak">Slabé</div>'
+                errorDiv.innerHTML = "";
+                document.getElementById("myButton").disabled = false;
             }
         }
+    });
+}
+
+function toggleImage(imageId) {
+    let image = document.getElementById(imageId);
+    let showButton = document.querySelector("button.spoiler-button[data-target='" + imageId + "']")
+    let hideButton = document.querySelector("button.hide-spoiler-button[data-target='" + imageId + "']")
+    if (image.style.display === "block") {
+        image.style.display = "none";
+        showButton.style.display = "block";
+        hideButton.style.display = "none";
+    } else {
+        image.style.display = "block";
+        showButton.style.display = "none";
+        hideButton.style.display = "block";
     }
 }
 
-function passwordStrengthNew() {
+async function themeDeleteConfirm() {
+    $(document).on('click', '.delete-theme', function () {
+        var themeId = $(this).data('id');
+        var confirm = window.confirm("Ste si istý, že chcete vymazať túto tému?");
+        if (confirm) {
+            var input = prompt("Zadajte 'admin' pre potvrdenie vymazania témy", "");
+            if (input === "admin") {
+                $.ajax({
+                    type: 'POST',
+                    url: '?c=forumThemes&a=delete',
+                    data: {id: themeId},
+                    success: function (data) {
+                        alert("Téma bola úspešne vymazaná");
+                        $("#theme-" + themeId).fadeOut(1000);
+                    },
+                    error: function (data) {
+                        alert("Chyba pri mazaní témy");
+                    }
+                });
+            } else {
+                alert("Nesprávne heslo, téma nebola vymazaná");
+            }
+        }
+    });
+}
+
+async function profileDeleteConfirm() {
+    $(document).on('click', '.delete-profile', function () {
+        var profileId = $(this).data('id');
+        var confirm = window.confirm("Ste si istý, že chcete vymazať tento profil?");
+        if (confirm) {
+            $.ajax({
+                type: 'POST',
+                url: '?c=auth&a=delete',
+                data: {id: profileId},
+                success: function (data) {
+                    alert("Profil bol úspešne vymazaný");
+                    window.location.href = "?c=auth&a=login"
+                },
+                error: function (data) {
+                    alert("Chyba pri mazaní profilu");
+                }
+            });
+        }
+    });
+}
+
+
+function tooltip() {
+    $(document).ready(function () {
+        $('#account_pic').tooltip();
+    });
+}
+
+function inputToImg() {
+    document.getElementById("account_pic").addEventListener("click", function () {
+        document.getElementById("photo").click();
+    });
+}
+
+function viewInputToImg() {
+    document.getElementById("photo").addEventListener("change", function () {
+        var file = this.files[0];
+        var reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+            // Nastavenie atribútu "src" elementu "img" na načítaný obrázok
+            document.getElementById("account_pic").src = this.result;
+        });
+
+        reader.readAsDataURL(file);
+    });
+}
+
+function defaultAccImg() {
+    var image = document.getElementById("account_pic");
+    image.onerror = function () {
+        this.src = "public/images/defaultAccImg.webp";
+    }
+}
+
+function passwordStrength() {
     const passwordInput = document.getElementById("password");
     const passwordStrengthIndicator = document.querySelector(
         ".password-strength-indicator .strength"
@@ -106,91 +202,19 @@ function passwordStrengthNew() {
     }
 }
 
-function passwordStrengthGradient() {
-    const passwordInput = document.getElementById("password");
-    const passwordStrengthIndicator = document.querySelector(
-        ".password-strength-indicator .strength"
-    );
-
-    passwordInput.addEventListener("input", updatePasswordStrength);
-
-    function updatePasswordStrength() {
-        const password = passwordInput.value;
-        const strengthRegex = new RegExp(
-            "^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$",
-            "g"
-        );
-
-        if (strengthRegex.test(password)) {
-            // Silné
-            passwordStrengthIndicator.classList.remove("weak", "medium");
-            passwordStrengthIndicator.classList.add("strong");
-            passwordStrengthIndicator.parentElement.style.backgroundSize = "100% 100%"; /* zobrazí sa pozadie pre silné heslo */
-        } else {
-            const mediumRegex = new RegExp(
-                "^(?=.{8,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$",
-                "g"
-            );
-
-            if (mediumRegex.test(password)) {
-                // Stredne silné
-                passwordStrengthIndicator.classList.remove("weak", "strong");
-                passwordStrengthIndicator.classList.add("medium");
-                passwordStrengthIndicator.parentElement.style.backgroundSize = "50% 100%";
-            } else {
-                // Slabé
-                passwordStrengthIndicator.classList.remove("medium", "strong");
-                passwordStrengthIndicator.classList.add("weak");
-                passwordStrengthIndicator.parentElement.style.backgroundSize = "25% 100%";
-            }
-        }
-    }
-}
-
-async function loginUserX() {
-    // Získame hodnoty z formulára pre používateľské meno a heslo
-    var username = $('#username').val();
-    var password = $('#password').val();
-
-    // Vytvoríme objekt s požiadavkou na server
-    try {
-        const data = await $.ajax({
-            url: '?c=auth&a=loginUser',
-            method: 'POST',
-            dataType: 'json',
-            data: {username, password},
-        });
-
-        // Ak bolo prihlásenie úspešné, presmerujeme používateľa na domovskú stránku
-        if (data.success) {
-            window.location.replace("?c=home");
-        } else {
-            // Ak nebolo prihlásenie úspešné, zobrazíme chybovú hlášku
-            $('#error-message').text(data.message);
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-async function loginUser() {
-    // Získame hodnoty z formulára pre používateľské meno a heslo
+async function loginUserCheck() {
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
 
-    // Vytvoríme objekt s požiadavkou na server
     try {
-        const response = await fetch(`?c=auth&a=loginUser&password=${password}&username=${username}`, {
+        const response = await fetch(`?c=auth&a=loginUserCheck&password=${password}&username=${username}`, {
             method: 'POST'
         });
 
         const data = await response.json();
 
-        // Ak bolo prihlásenie úspešné, presmerujeme používateľa na domovskú stránku
         if (data.success) {
-            window.location.href='?c=home';
         } else {
-            // Ak nebolo prihlásenie úspešné, zobrazíme chybovú hlášku
             document.querySelector('#error-message').textContent = data.message;
         }
     } catch (error) {
@@ -198,6 +222,136 @@ async function loginUser() {
     }
 }
 
+async function registerUsernameCheck(usernameInput) {
+    $("#" + usernameInput).keyup(debounce(async function () {
+        var username = $(this).val();
+        if (!username) {
+            $("#username_response").html("");
+            return;
+        }
+        $("#username_response").html("...");
+        var sessionUsername = $("#sessionUsername").val();
+        if (username) {
+            if (username.length < 4) {
+                $("#username_response").text("Používateľské meno musí mať aspoň 4 znaky");
+                return;
+            }
+            var response = await fetch('?c=auth&a=registerUsernameCheck', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                body: `username=${encodeURIComponent(username)}`
+            });
+            var data = await response.json();
+            if (data.taken && username !== sessionUsername) {
+                $("#username_response").text("Používateľské meno je obsadené");
+                document.getElementById("myButton").disabled = true;
+            } else if (!/^[a-zA-Z0-9]+$/.test(username)) {
+                $("#username_response").text("Používateľské meno môže obsahovať iba písmená a čísla");
+            } else {
+                $("#username_response").text("");
+                document.getElementById("myButton").disabled = false;
+            }
+        }
+    }, 500));
+}
+
+async function registerEmailCheck(emailInput) {
+    $("#" + emailInput).keyup(debounce(async function () {
+        var email = $(this).val();
+        if (!email) {
+            $("#email_response").html("");
+            return;
+        }
+        $("#email_response").html("...");
+        var sessionEmail = $("#sessionEmail").val();
+        var response = await fetch('?c=auth&a=registerEmailCheck', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: `email=${encodeURIComponent(email)}`
+        });
+        var data = await response.json();
+        if (data.taken && email !== sessionEmail) {
+            $("#email_response").html("E-mailová adresa je už zaregistrovaná");
+        } else {
+            if (data.notValid) {
+                $("#email_response").html("Zadaná e-mailová adresanie nie je validná");
+            } else if (email === sessionEmail) {
+                $("#email_response").html("Vaša e-mailová adresa je už zaregistrovaná");
+            } else {
+                $("#email_response").html("");
+            }
+        }
+    }, 500));
+}
+
+function registerNameCheck(nameInput) {
+    $("#" + nameInput).keyup(function () {
+        const name = $("#" + nameInput).val();
+        if (name !== "" && name.length < 4) {
+            $('#meno_response').text('Meno musí mať viac ako 4 znaky');
+        } else if (name !== "" && name.length > 20) {
+            $('#meno_response').text('Meno nesmie mať viac ako 20 znakov');
+        } else if (name !== "" && !/^[a-zA-Z\sÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽž]+$/.test(name)) {
+            $('#meno_response').text('Meno môže obsahovať len písmená');
+        } else {
+            $('#meno_response').text('');
+        }
+    });
+}
+
+function registerPasswordLengthCheck(passwordInput) {
+    $("#" + passwordInput).keyup(function () {
+        const password = $("#" + passwordInput).val();
+        if (password !== "" && password.length < 8) {
+            $('#password_response').text('Zadané heslo je príliš krátke');
+        } else {
+            $('#password_response').text('');
+        }
+    });
+}
+
+function registerPasswordsCheck() {
+    const password = $("#password").val();
+    const password2 = $("#password2").val();
+    if (password !== password2) {
+        $('#passwords_response').text('Zadané heslá sa nezhodujú');
+        document.getElementById("myButton").disabled = true;
+    } else {
+        $('#passwords_response').text('');
+        document.getElementById("myButton").disabled = false;
+    }
+}
+
+function registerHandler() {
+    $(document).ready(function () {
+        registerUsernameCheck("username");
+    });
+    $(document).ready(function () {
+        registerEmailCheck("email");
+    });
+    $(document).ready(function () {
+        registerNameCheck("meno");
+    });
+    $(document).ready(function () {
+        registerPasswordLengthCheck("password");
+    });
+    $(document).ready(function () {
+        $("#password").keyup(function () {
+            registerPasswordsCheck();
+        });
+        $("#password2").keyup(function () {
+            registerPasswordsCheck();
+        });
+    });
+}
+
+function debounce(fn, wait) {
+    let timeout;
+    return function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => fn.apply(this, arguments), wait);
+    };
+}
 
 
 
