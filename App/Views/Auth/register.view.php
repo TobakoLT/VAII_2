7ae4ca9bf611373c62c23b87b6e3243e47b8e4b8
TobakoLT <martin.hronec41@gmail.com>
2023-02-13 23:41:22 +0100

    Výpis kontroly platnosti údajov.

--- App/Views/Auth/register.view.php
+++ App/Views/Auth/register.view.php
@@ -13,40 +13,45 @@ $layout = 'auth';
                         <?= @$data['message'] ?>
                     </div>
                     <form method="post" class="form-signin" action="?c=auth&a=store">
-                        <div class="form-label-group">
+                        <div>
                             <label for="username">Používateľské meno</label>
                             <input type="text" id="username" name="username" class="form-control"
                                    placeholder="Používateľské meno" minlength="4" maxlength="25" required autofocus>
+                            <div id="username_response" class="form-error"></div>
 
                         </div>
-                        <div class="form-label-group mt-2">
+                        <div class="mt-2">
                             <label for="email">Email</label>
                             <input type="email" id="email" name="email" class="form-control"
-                                   placeholder="example@mail.com" minlength="5" maxlength="30" required>
-
+                                   placeholder="example@mail.com" maxlength="30" required>
+                            <div id="email_response" class="form-error"></div>
                         </div>
-                        <div class="form-label-group mt-2">
+                        <div class="mt-2">
                             <label for="meno">Celé meno</label>
                             <input type="text" id="meno" name="meno" class="form-control" placeholder="Meno Priezvisko"
-                                   minlength="5" maxlength="20" required>
-
+                                   minlength="4" maxlength="20" required>
+                            <div id="meno_response" class="form-error"></div>
                         </div>
-                        <div class="form-label-group mt-2">
+                        <div class="mt-2">
                             <label for="password">Heslo</label>
                             <input type="password" id="password" name="password" class="form-control"
                                    placeholder="Heslo" minlength="8" maxlength="30" required>
-
+                            <div id="password_response" class="form-error"></div>
                         </div>
-                        <div class="form-label-group">
+                        <div>
                             <input type="password" id="password2" name="password2" class="form-control mt-1"
                                    placeholder="Zopakovať heslo" maxlength="30" required>
+                            <div id="passwords_response" class="form-error"></div>
                         </div>
                         <div class="password-strength-indicator mt-2" id="password-strength-indicator">
                             <div class="strength"></div>
                             <input type="hidden" name="form_name" value="register_form">
                         </div>
-                        <button class="btn btn-lg btn-primary btn-block mt-3" type="submit" name="submit">Registrovať
-                        </button>
+                        <div class="d-flex justify-content-center">
+                            <button id="myButton" class="btn btn-lg btn-dark btn-block mt-3" type="submit"
+                                    name="submit">Registrovať
+                            </button>
+                            <div
                     </form>
                 </div>
             </div>
@@ -55,5 +60,10 @@ $layout = 'auth';
 </div>
 
 <script>
-    passwordStrength()
+
+    passwordStrength();
+    registerHandler();
+
+
 </script>
+
